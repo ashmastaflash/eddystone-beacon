0a06463162894c64b2462884b869bc5b3a767932
Ash Wilson <ash.d.wilson@gmail.com>
2016-09-11 16:58:03 -0700

    Activate interface before util starts

--- bin/runner.sh
+++ bin/runner.sh
@@ -1,7 +1,7 @@
 # Using hcitool to do things...
-# hciconfig hci0 down
-# hciattach /dev/ttyAMA0 bcm43xx 115200 noflow -
-# hciconfig hci0 up
+ hciconfig hci0 down
+ hciattach /dev/ttyAMA0 bcm43xx 115200 noflow -
+ hciconfig hci0 up
 # hciconfig hci0 leadv 3
 # hcitool -i hci0 cmd $BROADCAST_THIS
 
